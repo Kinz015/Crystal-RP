c21dbc9ac6cd1dd723ffb429caac38ecff367dd2
Guilherme Lins <guilhermelins978@gmail.com>
2023-08-19 12:42:57 -0300

    coloquei o erro da porcentagem

--- js/index.js
+++ js/index.js
@@ -19,6 +19,8 @@ const blocoResults = document.querySelector('.blocoResults')
 
 const copyBtn = document.createElement('button')
 
+const err = document.querySelector('#porcentError')
+
 lavarDinheiro.addEventListener('click', function calculate() {
 
         if (resultDinheiroPraMaquina.value.length  > 0) {
@@ -27,71 +29,72 @@ lavarDinheiro.addEventListener('click', function calculate() {
           resultDinheiroPraFaccao.value = ''
         }
 
-        // if (porcentagem.value < 30 || porcentagem.value > 100) {
-        //   console.log('')
-        // }
+        if (porcentagem.value < 30 || porcentagem.value > 100) {
+          err.textContent = 'O valor da porcentagem deve ser entre 30% e 100%'
+        } else {
+          err.textContent = ''
+          let valorDaPorcentagem = Number(porcentagem.value) * Number(dinheiroSujo.value) / 100
 
-        let valorDaPorcentagem = Number(porcentagem.value) * Number(dinheiroSujo.value) / 100
-
-        let dinheiroPraMaquina = Number(dinheiroSujo.value) * 15 / 100
-
-        let dinheiroProCliente = Number(dinheiroSujo.value) - Number(valorDaPorcentagem)
-        
-        let dinheiroPraFaccao = valorDaPorcentagem - dinheiroPraMaquina
-
-        let resultDinheiroPraMaquinaFormatted = new Intl.NumberFormat('en-US', {
-          currency: 'USD'
-        }).format(dinheiroPraMaquina)
-
-        resultDinheiroPraMaquina.disabled = true
-        resultDinheiroPraMaquina.value = `R$ ${resultDinheiroPraMaquinaFormatted}`
-        resultDinheiroPraMaquina.classList.add('resultDinheiro')
-        spanDinheiroPraMaquina.append(resultDinheiroPraMaquina)
-        divMaquina.append(spanDinheiroPraMaquina)
-
-        let resultDinheiroProClienteFormatted = new Intl.NumberFormat('en-US', {
-          currency: 'USD'
-        }).format(dinheiroProCliente)
-
-        resultDinheiroProCliente.disabled = true
-        resultDinheiroProCliente.value = `R$ ${resultDinheiroProClienteFormatted}`
-        resultDinheiroProCliente.classList.add('resultDinheiro')
-        spanDinheiroProCliente.append(resultDinheiroProCliente)
-        divCliente.append(spanDinheiroProCliente)
-
-        let resultDinheiroPraFaccaoFormatted = new Intl.NumberFormat('en-US', {
-          currency: 'USD'
-        }).format(dinheiroPraFaccao)
-
-        resultDinheiroPraFaccao.disabled = true
-        resultDinheiroPraFaccao.value = `R$ ${resultDinheiroPraFaccaoFormatted}`
-        resultDinheiroPraFaccao.classList.add('resultDinheiro')
-        spanDinheiroPraFaccao.append(resultDinheiroPraFaccao)
-        divFaccao.append(spanDinheiroPraFaccao)
-
-        blocoResults.classList.add('modificadBlocoResults')
-
-        console.log(valorDaPorcentagem)
-
-        copyBtn.classList.add('copy-btn')
-        copyBtn.textContent = 'Copiar'
-        spanBtnCopy.append(copyBtn)
-        
-        copyBtn.addEventListener('click', function (ev) {
-        const button = ev.currentTarget
-        if (button.innerText === "Copiar") {
-            button.innerText = "Copiado!"
-            button.classList.add("success")
-            navigator.clipboard.writeText(
-            `Valor total sujo: R$ ${dinheiroSujo.value}
-Porcentagem: R$ ${porcentagem.value}%
-Dinheiro pro cliente: R$ ${dinheiroProCliente} 
-Dinheiro pra facção: R$ ${dinheiroPraFaccao}`)
-          } else {
-            button.innerText = "Copiar"
-            button.classList.remove("success")
-          }
-    })  
+          let dinheiroPraMaquina = Number(dinheiroSujo.value) * 15 / 100
+  
+          let dinheiroProCliente = Number(dinheiroSujo.value) - Number(valorDaPorcentagem)
+          
+          let dinheiroPraFaccao = valorDaPorcentagem - dinheiroPraMaquina
+  
+          let resultDinheiroPraMaquinaFormatted = new Intl.NumberFormat('en-US', {
+            currency: 'USD'
+          }).format(dinheiroPraMaquina)
+  
+          resultDinheiroPraMaquina.disabled = true
+          resultDinheiroPraMaquina.value = `R$ ${resultDinheiroPraMaquinaFormatted}`
+          resultDinheiroPraMaquina.classList.add('resultDinheiro')
+          spanDinheiroPraMaquina.append(resultDinheiroPraMaquina)
+          divMaquina.append(spanDinheiroPraMaquina)
+  
+          let resultDinheiroProClienteFormatted = new Intl.NumberFormat('en-US', {
+            currency: 'USD'
+          }).format(dinheiroProCliente)
+  
+          resultDinheiroProCliente.disabled = true
+          resultDinheiroProCliente.value = `R$ ${resultDinheiroProClienteFormatted}`
+          resultDinheiroProCliente.classList.add('resultDinheiro')
+          spanDinheiroProCliente.append(resultDinheiroProCliente)
+          divCliente.append(spanDinheiroProCliente)
+  
+          let resultDinheiroPraFaccaoFormatted = new Intl.NumberFormat('en-US', {
+            currency: 'USD'
+          }).format(dinheiroPraFaccao)
+  
+          resultDinheiroPraFaccao.disabled = true
+          resultDinheiroPraFaccao.value = `R$ ${resultDinheiroPraFaccaoFormatted}`
+          resultDinheiroPraFaccao.classList.add('resultDinheiro')
+          spanDinheiroPraFaccao.append(resultDinheiroPraFaccao)
+          divFaccao.append(spanDinheiroPraFaccao)
+  
+          blocoResults.classList.add('modificadBlocoResults')
+  
+          console.log(valorDaPorcentagem)
+  
+          copyBtn.classList.add('copy-btn')
+          copyBtn.textContent = 'Copiar'
+          spanBtnCopy.append(copyBtn)
+          
+          copyBtn.addEventListener('click', function (ev) {
+          const button = ev.currentTarget
+          if (button.innerText === "Copiar") {
+              button.innerText = "Copiado!"
+              button.classList.add("success")
+              navigator.clipboard.writeText(
+              `Valor total sujo: R$ ${dinheiroSujo.value}
+  Porcentagem: R$ ${porcentagem.value}%
+  Dinheiro pro cliente: R$ ${dinheiroProCliente} 
+  Dinheiro pra facção: R$ ${dinheiroPraFaccao}`)
+            } else {
+              button.innerText = "Copiar"
+              button.classList.remove("success")
+            }
+      })
+        }    
 })
 
 
